2e9d73f2026cfda58374e5808bb24564e18c19c9
mymoney <117241747+frisca30@users.noreply.github.com>
2024-09-23 21:11:20 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, requests, time, crayons
+import os, requests, time, crayons, json
 
 def print_banner():
     print(crayons.blue('█     █ █  █ ▄▄▄▄▄ █  █ ▄▄▄▄▄'))
@@ -7,18 +7,19 @@ def print_banner():
     print()
 
 
+
 class MoonBix:
     def __init__(self, token, proxy=None):
         self.session = requests.session()
         self.session.headers.update({
-            'authority': 'www.binance.com',
+            'authority': 'www.binance.info',
             'accept': '*/*',
             'accept-language': 'en-EG,en;q=0.9,ar-EG;q=0.8,ar;q=0.7,en-GB;q=0.6,en-US;q=0.5',
             'clienttype': 'web',
             'content-type': 'application/json',
             'lang': 'en',
-            'origin': 'https://www.binance.com',
-            'referer': 'https://www.binance.com/en/game/tg/moon-bix',
+            'origin': 'https://www.binance.info',
+            'referer': 'https://www.binance.info/en/game/tg/moon-bix',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36',
         })
         
@@ -29,50 +30,71 @@ class MoonBix:
         self.game_response = None
 
     def login(self):
-        response = self.session.post(
-            'https://www.binance.com/bapi/growth/v1/friendly/growth-paas/third-party/access/accessToken',
-            json={'queryString': self.token, 'socialType': 'telegram'},
-        )
-        if response.status_code == 200:
-            self.session.headers['x-growth-token'] = response.json()['data']['accessToken']
-            return True
-        return False
+        try:
+            response = self.session.post(
+                'https://www.binance.info/bapi/growth/v1/friendly/growth-paas/third-party/access/accessToken',
+                json={'queryString': self.token, 'socialType': 'telegram'},
+            )
+            if response.status_code == 200:
+                self.session.headers['x-growth-token'] = response.json()['data']['accessToken']
+                return True
+            return False
+        except Exception as e: 
+            print(crayons.red(f"Error during login: {e}")) 
 
     def user_info(self):
-        response = self.session.post(
-            'https://www.binance.com/bapi/growth/v1/friendly/growth-paas/mini-app-activity/third-party/user/user-info',
-            json={'resourceId': 2056},
-        )
-        return response.json()
-
-    def game_data(self):
-        while True:
-            response = requests.post('https://vemid42929.pythonanywhere.com/api/v1/moonbix/play', json=self.game_response).json()
-            if response['message'] == 'success' and response['game']['log'] >= 100:
-                self.game = response['game']
-                return True
-
-    def complete_game(self):
-        response = self.session.post(
-            'https://www.binance.com/bapi/growth/v1/friendly/growth-paas/mini-app-activity/third-party/game/complete',
-            json={'resourceId': 2056, 'payload': self.game['payload'], 'log': self.game['log']},
-        )
-        return response.json()['success']
-
-    def start_game(self):
-        while True:
+        try:
             response = self.session.post(
-                'https://www.binance.com/bapi/growth/v1/friendly/growth-paas/mini-app-activity/third-party/game/start',
+                'https://www.binance.info/bapi/growth/v1/friendly/growth-paas/mini-app-activity/third-party/user/user-info',
                 json={'resourceId': 2056},
             )
-            self.game_response = response.json()
-            if self.game_response['code'] == '000000':
-                return True
-            elif self.game_response['code'] == '116002':
-                print(crayons.red('Attempts not enough! Switching to the next account.'))
+            return response.json()
+        
+        except Exception as e: 
+            print(crayons.red(f"Error during get info: {e}")) 
+
+    def game_data(self):
+        try:
+            while True:
+                responses = requests.post('https://app.winsnip.xyz/play', json=self.game_response).text
+                try:
+                    response = json.loads(responses)
+                except json.JSONDecodeError:
+                    continue
+                if response['message'] == 'success' and response['game']['log'] >= 100:
+                    self.game = response['game']
+                    return True
+        except Exception as e: 
+            print(crayons.red(f"Error get gamedata: {e}"))
+
+    def complete_game(self):
+        try:
+            response = self.session.post(
+                'https://www.binance.info/bapi/growth/v1/friendly/growth-paas/mini-app-activity/third-party/game/complete',
+                json={'resourceId': 2056, 'payload': self.game['payload'], 'log': self.game['log']},
+            )
+            return response.json()['success']
+        except Exception as e: 
+            print(crayons.red(f"Error during complete game: {e}")) 
+
+    def start_game(self):
+        try:
+            while True:
+                response = self.session.post(
+                    'https://www.binance.info/bapi/growth/v1/friendly/growth-paas/mini-app-activity/third-party/game/start',
+                    json={'resourceId': 2056},
+                )
+                self.game_response = response.json()
+                if self.game_response['code'] == '000000':
+                    return True
+                elif self.game_response['code'] == '116002':
+                    print(crayons.red('Attempts not enough! Switching to the next account.'))
+                    return False
+                print(crayons.red("ERROR! Cannot start game."))
                 return False
-            print(crayons.red("ERROR! Cannot start game."))
-            return False
+            
+        except Exception as e: 
+            print(crayons.red(f"Error during start game: {e}")) 
 
     def start(self):
         if not self.login():
@@ -110,9 +132,7 @@ def sleep(seconds):
 if __name__ == '__main__':
     os.system('cls' if os.name == 'nt' else 'clear')
     print_banner()
-    #use_proxy = 'y'
-    #proxies = [line.strip() for line in open('proxy.txt')] if use_proxy == 'y' else []
-    proxies = [line.strip() for line in open('proxy.txt') if line.strip()]
+    proxies = [line.strip() for line in open('proxy.txt') if line.strip()]  
     print(crayons.yellow("============================= Starting =============================="))
     while True:
         tokens = [line.strip() for line in open('data.txt')]
@@ -125,4 +145,4 @@ if __name__ == '__main__':
             sleep(15)
 
         print(crayons.green("All accounts have been completed"))
-        sleep(3600)
+        sleep(2000)
